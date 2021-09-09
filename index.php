cb8f93d7b53e2c530173dac0e28f38d9873845ef
Humberto <humber16.02@gmail.com>
2021-09-09 15:44:25 -0300

    Lesson 5: separate php logic from presentation

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        header{
-            background-color: #3e3e3e;
-            color: aliceblue;
-            padding: 2em;
-            text-align: center
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?php 
-            
-                $name = htmlspecialchars($_GET['name']);
-                $surname = htmlspecialchars($_GET['surname']);
-                echo "Hello, {$name} {$surname}"; 
-                
-            ?>
-        </h1>
-    </header>
-</body>
-</html>
+<?php 
+
+$name = htmlspecialchars($_GET['name']);
+$surname = htmlspecialchars($_GET['surname']);
+$greeting = "Hello, $name $surname";
+
+require 'index.view.php'; /* de esta forma hacemos la referencia para que la vista haga a la variable */
