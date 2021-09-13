67a33562deb1dc95737d52abc4b1d68aa23597f5
Humberto <humber16.02@gmail.com>
2021-09-13 12:32:36 -0300

    Lesson7: Associative arrays

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php 
 
-$names = ['John','Mary','Lucas'];
+// arreglo asociativo, clave valor
+$person = [
+    'age' => 25,
+    'hair' => 'brown',
+    'career' => 'web dev'
+];
+
+$person['name'] = 'Humberto'; //así agregamos elementos
+unset($person['hair']); //así los quitamos
+
+echo('<pre>'); //para que sea más presentable
+var_dump($person); //echo sirve solo para strings, así podemos "ver" la variable
+echo('</pre>');
+die(); //detenemos la ejecución, se puede poner todo junto con la de arriba
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -16,17 +16,9 @@
 </head>
 <body>
     <ul>
-        <!-- #1: Abrimos el foreach y metemos el html, cuesta escalar este código -->
-        <?php
-            foreach ($names as $name){
-                echo "<li>$name</li>";
-        }
-        ?>
-
-        <!-- #2 -->
-        <?php foreach ($names as $name): ?><!-- una para abrir, notar los dos pts -->
-            <li><?= $name ?></li> <!-- html. Php solo cuando se lo necesite -->
-        <?php endforeach ?> <!-- el cierre -->
+        <?php foreach($person as $key => $feature): ?> <!-- así referenciamos la clave valor, si deseamos mostrar ambas. Sino, mostraría solo valor -->
+            <li><strong><?= $key ?></strong> <?= $feature ?></li>
+        <?php endforeach ?>
     </ul>
 </body>
 </html>
