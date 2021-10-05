5dcd8334b1fbc77978b1b3c0f256588097401ed1
Humberto <humber16.02@gmail.com>
2021-10-05 12:12:11 -0300

    Lesson21: Composer Autoloading

--- core/bootsrap.php
+++ core/bootsrap.php
@@ -4,10 +4,7 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+//ya no necesito mil requires aca, todo lo hace el autoloader en index
 
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database'])
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 
+require 'vendor/autoload.php'; //no olvidar este require, ya no hace falta controlar nada más
 //ahora el bootstrap.php se encarga de crear la conexión y servir la db listo para que consultemos, es decir, pasemos métodos
 require 'core/bootsrap.php';
 
